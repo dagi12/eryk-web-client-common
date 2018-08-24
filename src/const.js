2779895f76a56c064645392ca172ceaf62bf1e10
Eryk Mariankowski <dagi12@o2.pl>
2018-08-24 12:00:14 +0200

    003307 - przycisk czyszczenia datepickera nie powinien być widoczny w trybie podglądu - bardzo wolne podstawienie podpowiedzi po wyborze samochodu w kreatorze dodania zlecenia na ekranie szczegółów - typy interwencji nie powinny się czyścić po wyczyszczeniu autocomplete'u - zmień komunikat "no results found" na "brak wyników" na selectcie - zamień paginacje na infinite scroll - ikona panolka w górnym lewym rogu

--- src/const.js
+++ src/const.js
@@ -1,5 +1,6 @@
 export const UPPER_CHAR_REGEXP = new RegExp('[A-Z]');
 export const LOWER_CHAR_REGEXP = new RegExp('[a-z]');
-export const SPECIAL_CHAR_REGEXP = /^(?=.*[$@$!%*?&#])/i;
+export const SPECIAL_CHAR_REGEXP = /^(?=.*[$@!%*?&#])/i;
 export const DIGIT_REGEXP = /^(?=.*\d)/i;
 export const PASSWORD_LENGTH = 8;
+export const NAV_WIDTH = '240px';
